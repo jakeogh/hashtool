9924f41d70352418be39f5a8ae2aa8340eedb8b6
jakeogh <github.com@v6y.net>
2021-08-07 14:49:06 -0700

    auto-commit

--- hashtool/__init__.py
+++ hashtool/__init__.py
@@ -3,7 +3,6 @@ from .hashtool import emptyhash
 from .hashtool import generate_hashlib_algorithm_set
 from .hashtool import hash_bytes
 from .hashtool import hash_file
-from .hashtool import hash_file_handle
 from .hashtool import hexdigest_str_path
 from .hashtool import hexdigest_str_path_relative
 from .hashtool import rhash_file
