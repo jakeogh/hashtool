b279aa9644f813368abe1f7c56807d5ed41ecb96
jakeogh <github.com@v6y.net>
2022-10-04 14:39:41 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ import fastentrypoints
 if not sys.version_info[0] == 3:
     sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
 
-dependencies = ["icecream", "click", "colorama"]
+dependencies = [
+    "advisory-lock @ git+https://git@github.com/jakeogh/advisory_lock",
+]
 
 config = {
     "version": "0.1",
