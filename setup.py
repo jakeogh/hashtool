86a96874b9cb7b1021708a46e4d82110792e1a50
jakeogh <github.com@v6y.net>
2022-10-26 20:20:30 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import sys
 
 from setuptools import find_packages
 from setuptools import setup
 
 import fastentrypoints
-
-if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
 
 dependencies = [
     "advisory-lock @ git+https://git@github.com/jakeogh/advisory_lock",
@@ -28,7 +24,7 @@ config = {
     "license": "ISC",
     "author": "Justin Keogh",
     "author_email": "github.com@v6y.net",
-    "description": "Short explination of what it does _here_",
+    "description": "common hash functions",
     "long_description": __doc__,
     "packages": find_packages(exclude=["tests"]),
     "package_data": {"hashtool": ["py.typed"]},
